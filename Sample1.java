bb3edbf306be3b27ad784de68027d98248458030
tanii-shio <shioya.naoto17@gmail.com>
2023-10-20 04:34:47 +0000

    demo compile

---
+++ Sample1.java
@@ -0,0 +1,18 @@
+class Sample1 {
+  public static void main(String[] args) {
+    int num1 = 100;
+    int num2, num3;
+    String str1;
+
+    num2 = 200;
+    num3 = 300;
+    str1 = "Good Bye!";
+
+    System.out.println("Hello Java World");
+    System.out.println(500);
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println("num3の値は " + num3 + " です");
+    System.out.println(str1);
+  }
+}
