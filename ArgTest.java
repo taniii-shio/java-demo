3e4e9e7bf2dc56fcd508456dcda4a2f79bf1bcfd
naoto <shioya.naoto17@gmail.com>
2023-10-26 02:30:12 +0000

    referrer val test

---
+++ ArgTest.java
@@ -0,0 +1,21 @@
+public class ArgTest {
+  public static void main(String[] args) {
+    MyClass obj = new MyClass();
+    int aa = 100;
+    obj.val = 100;
+    // 引数での変数の受け渡し（呼び出す側の変数を関数内の変数に代入）
+    // funcB(aa, obj);
+
+    // 代入の場合
+    MyClass obj2 = obj;
+    obj2.val = 200;
+
+    System.out.println("aa : " + aa);
+    System.out.println("obj.val : " + obj.val);
+  }
+
+  public static void funcB(int aa, MyClass obj) {
+    aa = 200;
+    obj.val = 200;
+  }
+}
